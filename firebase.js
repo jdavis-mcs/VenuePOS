9a9fed037cafda2ffc65bd01fc490bd5ce82a616
jdavis-mcs <jdavis@madison.k12.in.us>
2026-02-05 16:06:54 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, doc, getDocs, addDoc, deleteDoc, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// --- PASTE YOUR CONFIG HERE ---
+const firebaseConfig = {
+  apiKey: "AIzaSyAlM8S1unToP0ALIheaSRa7KC7jqKIPyPg",
+  authDomain: "venuepos.firebaseapp.com",
+  projectId: "venuepos",
+  storageBucket: "venuepos.firebasestorage.app",
+  messagingSenderId: "328950384760",
+  appId: "1:328950384760:web:8967dd2b16c5b88480058d",
+  measurementId: "G-71PSSCVD73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Enable Anonymous Auth immediately so rules pass
+signInAnonymously(auth).then(() => {
+    console.log("System: Connected to Backend");
+}).catch((error) => {
+    console.error("Auth Error", error);
+});
+
+// Export tools for other files to use
+export { db, collection, doc, getDocs, addDoc, deleteDoc, query, where, onSnapshot };
